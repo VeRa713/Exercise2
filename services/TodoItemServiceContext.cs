8ac8c3b0bdf1926404f09c7da3a8057fd6ffa19f
Vera David <vera.david1214@gmail.com>
2023-01-25 14:12:18 +0800

    implemented interfaces to services

--- services/TodoItemServiceContext.cs
+++ services/TodoItemServiceContext.cs
@@ -1,12 +1,30 @@
 using Exercise2.Models;
-using Exercise2.Conf;
+using Exercise2.Interfaces;
 
 namespace Exercise2.Services
 {
-    public class TodoItemServiceContext
+    public class TodoItemServiceContext : ITodoListService
     {
         // 5.	Create a service that implements ITodoItemService called TodoItemServiceContext 
         // which interacts with ApplicationContext to implement the underlying methods.
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
 
+        public TodoList FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoList> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoList Save(TodoList list)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- services/TodoListServiceContext.cs
+++ services/TodoListServiceContext.cs
@@ -1,11 +1,30 @@
 using Exercise2.Models;
-using Exercise2.Conf;
+using Exercise2.Interfaces;
 
 namespace Exercise2.Services
 {
-    public class TodoListServiceContext
+    public class TodoListServiceContext : ITodoListService
     {
         // 4.	Create a service that implements ITodoListService called TodoListServiceContext 
         // which interacts with ApplicationContext to implement the underlying methods.
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoList FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoList> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoList Save(TodoList list)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
